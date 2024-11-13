b59a1d2fb90da2a73d6a12f5ed434e7fac4ce8a1
Dreams Obasohan <jtp7d03u@students.codeinstitute.net>
2024-11-13 18:16:30 +0000

    add created created variables array of questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+
+// Arrays of questions and optional answers//
+const questions = [
+    {
+        question: "What is 176 - 18 ?",
+        answers: [
+            { text: "158", correct: true },
+            { text: "138", correct: false },
+            { text: "140", correct: false },
+            { text: "161", correct: false },
+        ]
+    },
+
+    {
+        question: "What is the capital of Spain?",
+        answers: [
+            { text: "Barcelona", correct: false },
+            { text: "Alicante", correct: false },
+            { text: "Madrid", correct: true },
+            { text: "Bilbao", correct: false },
+        ]
+    },
+    {
+        question: "Who is a football player ?",
+        answers: [
+            { text: "Michael Jackson", correct: false },
+            { text: "Cristiano Ronaldo", correct: true },
+            { text: "Hussain Bolt", correct: false },
+            { text: "Joe Biden", correct: false },
+        ]
+    },
+    {
+        question: "Who is a known singer ?",
+        answers: [
+           
+            { text: "Mohammed Salah", correct: false },
+            { text: "Alex Ferguson", correct: false },
+            { text: "Alan Shearer", correct: false },
+            { text: "Bayonce", correct: true },
+        ]
+    },
+    {
+        question: "What is the name of UK currency ?",
+        answers: [        
+            { text: "Dollar", correct: false },
+            { text: "Naira", correct: false },
+            { text: "Pounds Sterling", correct: true },
+            { text: "Euro", correct: false },
+        ]
+    },
+    {
+        question: "Where is Spain located ?",
+        answers: [
+            { text: "Africa", correct: false },
+            { text: "Australia", correct: false },
+            { text: "Asia", correct: false },
+            { text: "Europe", correct: true },
+        ]
+    },
+    {
+        question: "Who is a boxer ?",
+        answers: [
+            { text: "Harry Kane", correct: false },
+            { text: "Justin Bieber", correct: false },
+            { text: "Anthony Joshua", correct: true },
+            { text: "Rishi Sunak", correct: false },
+        ]
+    },
+]
+
